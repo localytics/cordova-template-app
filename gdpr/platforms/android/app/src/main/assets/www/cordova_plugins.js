f2f66d44a8fdc9b4c49625479f661b8bca308180
Michael Riegelhaupt <mriegel@alum.mit.edu>
2018-04-16 13:06:32 +0200

    Move around template app and add GDPR sample

---
+++ gdpr/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "com.localytics.phonegap.LocalyticsPlugin.Localytics",
+    "file": "plugins/com.localytics.phonegap.LocalyticsPlugin/www/localytics.js",
+    "pluginId": "com.localytics.phonegap.LocalyticsPlugin",
+    "clobbers": [
+      "Localytics"
+    ]
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-whitelist": "1.3.3",
+  "com.localytics.phonegap.LocalyticsPlugin": "1.0.0"
+};
+// BOTTOM OF METADATA
+});
